11b7e9732a19b5b5ab0df534601839190e8c037d
Sumanth Chinthagunta <xmlking@gmail.com>
2023-01-02 15:39:20 -0800

    feat(houdini): add houdini graphql-ws client

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -1,24 +1,23 @@
 import { browser } from '$app/environment';
-import { env as dynPriEnv } from '$env/dynamic/private';
 import { env as dynPubEnv } from '$env/dynamic/public';
 import type { RequestHandlerArgs } from '$houdini';
 import { HoudiniClient } from '$houdini';
 import { createClient as createWSClient } from 'graphql-ws';
 
 const url = dynPubEnv.PUBLIC_GRAPHQL_ENDPOINT;
-// FIXME: remove HASURA_GRAPHQL_ADMIN_SECRET
-const adminSecret = dynPriEnv.HASURA_GRAPHQL_ADMIN_SECRET;
-console.log('HASURA_GRAPHQL_ADMIN_SECRET', adminSecret);
+// FIXME: remove PUBLIC_API_TOKEN
+const adminSecret = dynPubEnv.PUBLIC_API_TOKEN ?? '';
+
 // For Query & Mutation
 async function fetchQuery({ fetch, text = '', variables = {}, metadata, session }: RequestHandlerArgs) {
 	const token = session?.user?.token;
-	console.log('HASURA_GRAPHQL_ADMIN_SECRET, token', adminSecret, token);
+
 	const result = await fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 			...(token ? { Authorization: `Bearer ${token}` } : {}),
-			'X-Hasura-Admin-Secret': adminSecret // FIXME: remove
+			'X-Hasura-Admin-Secret': adminSecret // FIXME: remove me
 		},
 		body: JSON.stringify({
 			query: text,
