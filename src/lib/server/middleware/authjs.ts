315d2c418621a1fe877cbe02559ee56044f77612
Sumanth Chinthagunta <xmlking@gmail.com>
2023-01-22 17:36:32 -0800

    feat(hasura): now using dynamic auth token for hasure

--- src/lib/server/middleware/authjs.ts
+++ src/lib/server/middleware/authjs.ts
@@ -64,10 +64,10 @@ export const authjs = SvelteKitAuth({
 				const hasuraToken = {
 					//  role can be temporarily or permanently applied to a user to give the user bulk permissions for a task.
 					'https://hasura.io/jwt/claims': {
-						'X-Hasura-Allowed-Roles': ['viewer', 'editor', 'moderator', 'supervisor'], // user.roles,
-						'X-Hasura-Default-Role': 'viewer', // user.roles[0],
-						'X-Hasura-Org-Id': 'chinthagunta.com',
-						'X-Hasura-User-Id': token.email // token.sub
+						'x-hasura-allowed-roles': ['viewer', 'editor', 'moderator', 'supervisor'], // user.roles,
+						'x-hasura-default-role': 'viewer', // user.roles[0],
+						'x-hasura-org-id': 'chinthagunta.com',
+						'x-hasura-user-id': token.email // token.sub
 					}
 				};
 				token.token = jsonwebtoken.sign(hasuraToken, envPri.AUTH_SECRET, {
@@ -76,9 +76,10 @@ export const authjs = SvelteKitAuth({
 					expiresIn: account?.expires_in
 				});
 				// token.accessToken = account.access_token; // account.id_token
+
 				// FIXME: for Azure AD picture is base64 and it is too big to fit in cookie.
 				// will through `431 Request Header Fields Too Large` unless we remove it.
-				if (token.picture?.startsWith('data:')) delete token.picture;
+				// if (token.picture?.startsWith('data:')) delete token.picture;
 			}
 			return token;
 		},
