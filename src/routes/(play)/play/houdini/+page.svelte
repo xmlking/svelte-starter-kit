51df4fa93121ba95dd357bfb0e24123a03527614
Sumanth Chinthagunta <xmlking@gmail.com>
2023-03-04 13:30:30 -0800

    fix(members): adding error handling

--- src/routes/(play)/play/houdini/+page.svelte
+++ src/routes/(play)/play/houdini/+page.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
-	import { CachePolicy } from '$houdini';
-	import type { PageData } from './$houdini';
+		import { CachePolicy } from '$houdini';
+		import { ErrorMessage, GraphQLErrors } from '$lib/components';
+		import type { PageData } from './$houdini';
 
 	export let data: PageData;
-	$: ({ ListPolicies2 } = data);
+	$: ({  ListPolicies2, formErrors, fieldErrors } = data);
 </script>
 
 <button class="btn" on:click={() => ListPolicies2.loadNextPage()}>load more</button>
 <button class="btn-accent btn" on:click={() => ListPolicies2.fetch({ policy: CachePolicy.NetworkOnly })}>refetch</button>
 
+<ErrorMessage error={fieldErrors?.limit?.[0]} />
+<ErrorMessage error={fieldErrors?.offset?.[0]} />
+<ErrorMessage error={fieldErrors?.orderBy?.[0]} />
+<ErrorMessage error={fieldErrors?.displayNme?.[0]} />
+
 {#if $ListPolicies2.fetching}
 	Loading...
+{:else if  $ListPolicies2.errors}
+	<GraphQLErrors errors={$ListPolicies2.errors} />
 {:else}
 	<details>
 		<summary>raw data with @cache(policy: CacheAndNetwork)</summary>
