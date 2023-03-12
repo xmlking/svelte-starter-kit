38a7814af3e9bf2f188d567cadc621c2a24616dd
Sumanth Chinthagunta <xmlking@gmail.com>
2023-03-12 14:16:10 -0700

    fix: resue authjs token for hasura to slim the cookie size

--- src/lib/server/middleware/authjs-helper.ts
+++ src/lib/server/middleware/authjs-helper.ts
@@ -1,15 +1,25 @@
 import envPri from '$lib/variables/variables.server';
 import { decode } from '@auth/core/jwt';
 import type { Cookies } from '@sveltejs/kit';
+import { SignJWT } from 'jose';
 
-// FIXME: temp workarround. remove when fixed.
+// FIXME: temp workaround. remove when fixed.
 // https://github.com/nextauthjs/next-auth/discussions/5595#discussioncomment-4628977
 
 const secureCookie = envPri.NEXTAUTH_URL?.startsWith('https://') ?? envPri.VERCEL;
 const cookieName = secureCookie ? '__Secure-next-auth.session-token' : 'next-auth.session-token';
+const secret = new TextEncoder().encode(envPri.AUTH_SECRET);
+const alg = 'HS256';
 
 export async function getToken(cookies: Cookies, raw = false) {
 	const token = cookies.get(cookieName);
 	if (raw) return token;
 	return await decode({ token, secret: envPri.AUTH_SECRET });
+}
+
+export async function getSignedToken(cookies: Cookies) {
+	const decodedToken = await getToken(cookies);
+	if (decodedToken == null || typeof decodedToken === 'string') return null;
+	const signedToken = new SignJWT(decodedToken).setProtectedHeader({ alg }).sign(secret);
+	return signedToken;
 }
--- src/lib/server/middleware/houdini.old.ts
+++ src/lib/server/middleware/houdini.old.ts
@@ -2,7 +2,7 @@ import { building } from '$app/environment';
 import { setSession } from '$houdini';
 import type { Handle } from '@sveltejs/kit';
 // import { getToken } from '@auth/core/jwt';
-import { getSignedToken } from '$lib/server/middleware/authjs-helper';
+// import { getToken } from '$lib/server/middleware/authjs-helper';
 import { Logger } from '$lib/utils';
 
 const log = new Logger('middleware:houdini');
@@ -11,14 +11,14 @@ export const houdini = (async ({ event, resolve }) => {
 	// FIXME: https://github.com/nextauthjs/next-auth/discussions/6186
 	if (building) return await resolve(event);
 
-	const { cookies } = event;
+	const { locals, cookies } = event;
+	const { token } = (await locals.getSession()) ?? {};
 
 	// FIXME: always return null with @auth/core/jwt's getToken
 	// const token = await getToken({req: { cookies: event.cookies, headers: event.request.headers },secret: dynPriEnv.AUTH_SECRET,raw: true});
 	// const token = await getToken({ req: request, secret: dynPriEnv.AUTH_SECRET, raw: true });
 	// const token = await getToken(cookies);
 	// const token = await getToken(cookies, true); // raw = true
-	const token = await getSignedToken(cookies);
 	log.debug('setting token......', token);
 	if (token) setSession(event, { token });
 	const response = await resolve(event);
