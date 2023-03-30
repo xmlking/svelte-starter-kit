6628985f123b1c830734e333134497450ab0a6ff
Sumanth Chinthagunta <xmlking@gmail.com>
2023-03-30 08:57:21 -0700

    fix: update deps

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,5 @@ export { decrypt, encrypt } from './encrypt';
 export { default as generateId } from './generateId';
 export { Logger } from './logger';
 export { default as pkceChallenge } from './pkce';
+export { startsWith } from './strings';
+
