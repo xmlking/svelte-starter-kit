45cd55b49ba91d30e187c4aef9015d4e6ee81270
Sumanth Chinthagunta <xmlking@gmail.com>
2023-05-15 02:22:38 -0700

    fix(hasura): refactor organization

---
+++ infra/hasura/seeds/default/1684142149809_action_seed.sql
@@ -0,0 +1,7 @@
+SET check_function_bodies = false;
+INSERT INTO public.action (value, description) VALUES ('action_unspecified', 'ACTION_UNSPECIFIED');
+INSERT INTO public.action (value, description) VALUES ('action_block', 'ACTION_BLOCK');
+INSERT INTO public.action (value, description) VALUES ('action_callout_unknown', 'ACTION_CALLOUT_UNKNOWN');
+INSERT INTO public.action (value, description) VALUES ('action_permit', 'ACTION_PERMIT');
+INSERT INTO public.action (value, description) VALUES ('action_callout_inspection', 'ACTION_CALLOUT_INSPECTION');
+INSERT INTO public.action (value, description) VALUES ('action_callout_terminating', 'ACTION_CALLOUT_TERMINATING');
