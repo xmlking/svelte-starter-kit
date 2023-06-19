d7da4aaf8562a0cfa15ce1d1becdc10368d7d30a
Sumanth Chinthagunta <xmlking@gmail.com>
2023-06-19 00:37:06 -0700

    fix(hasura): fix migrations

--- infra/hasura/migrations/default/1686378049757_insert_seed_type_data/down.sql
+++ infra/hasura/migrations/default/1686378049757_insert_seed_type_data/down.sql
@@ -1,5 +1,13 @@
 SET rules.soft_deletion TO off;
 --
+DELETE FROM public.policies;
+--
+DELETE FROM public.pools;
+--
+DELETE FROM public.rules;
+--
+DELETE FROM public.devices;
+--
 DELETE FROM public.direction;
 --
 DELETE FROM public.action;
