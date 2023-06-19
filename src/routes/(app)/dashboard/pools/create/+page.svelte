be8ea9a6f5d362375350fd4c3eeeb9c17674dfe5
Sumanth Chinthagunta <xmlking@gmail.com>
2023-06-19 14:27:25 -0700

    fix: cleanup

--- src/routes/(app)/dashboard/pools/create/+page.svelte
+++ src/routes/(app)/dashboard/pools/create/+page.svelte
@@ -6,10 +6,9 @@
 	import { Breadcrumb, BreadcrumbItem, Heading, Helper } from 'flowbite-svelte';
 	import { superForm } from 'sveltekit-superforms/client';
 	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
-	import type { PageData } from './$types';
 
 	const log = new Logger('routes:pools:create');
-	export let data: PageData;
+	export let data;
 	// Client API:
 	const superform = superForm(data.form, {
 		dataType: 'json',
